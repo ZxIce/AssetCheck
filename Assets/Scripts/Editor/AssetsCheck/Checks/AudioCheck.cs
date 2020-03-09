2cf6366e450aceb40f1f1356ecbfc0e25461e5de
izzyzheng <695606786@qq.com>
2020-03-09 10:15:23 +0800

    音频单通道修复

---
+++ Assets/Scripts/Editor/AssetsCheck/Checks/AudioCheck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioCheck : ICheck
+{
+    private string _searchTag = "t:AudioClip";
+    public string SearchTag
+    {
+        get { return _searchTag;}
+        set { _searchTag = value; }
+    }
+    private bool _canFix = true;
+    public bool CanFix
+    {
+        get { return _canFix;}
+        set { _canFix = value; }
+    }
+    public bool Check(string path)
+    {
+        AudioImporter audioImporter =  AssetImporter.GetAtPath(path) as AudioImporter;
+        if (audioImporter == null)
+        {
+            return true;
+        }
+        
+        bool right = true;
+        
+        if (!audioImporter.forceToMono)
+        {
+            right = false;
+        }
+
+        return right;
+    }
+
+    public bool Fix(string path)
+    {
+        AudioImporter audioImporter =  AssetImporter.GetAtPath(path) as AudioImporter;
+        if (audioImporter == null)
+        {
+            return true;
+        }
+
+        audioImporter.forceToMono = true;
+        audioImporter.SaveAndReimport();
+        return true;
+    }
+}
